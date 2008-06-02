fd4967b1a1d4b51bfd7a6e5874bd77ddf5a2f6ab
Ion Morega <ion.morega@gmail.com>
2008-06-02 19:58:40 +0300

    updated utils.py to generate nice stats; updated parallel_1.py to use the new utils

--- parallel_1.py
+++ parallel_1.py
@@ -1,39 +1,55 @@
-from logic import LogCounter
-from smart_read import seek_open
+import pprocess
+import os
+import time
 
-def read_log_file(file_name, start, end):
+from logic import LogCounter
+from utils import seek_open, file_offsets, parse_argv, Stats
+
+def parse_chunk(file_name, start, end):
+    start_time = time.clock()
+    
     f = seek_open(file_name, start, end)
-    if start > 0:
-        f.next() # skip the first line, it's being read with the previous block
     
     counter = LogCounter()
     for line in f:
         counter.parse_line(line)
     
-    return counter
+    return (counter, os.getpid(), time.clock() - start_time)
 
-def file_offsets(file_size):
-    cuts = range(0, file_size, file_size/10)
-    cuts[10] = file_size
-    return map(lambda s, e: (s, e), cuts[:-1], cuts[1:])
-
-def main():
-    import sys
-    import os
+def parse_file(file_name, cores, jobs_per_core, stats):
+    file_size = os.path.getsize(file_name)
+    chunks = int(cores * jobs_per_core)
+    stats.initial_report(file_name, file_size, chunks)
     
-    file_name = sys.argv[1]
+    queue = pprocess.Queue(limit=cores)
+    parse_chunk_async = queue.manage(pprocess.MakeParallel(parse_chunk))
+    
+    for (start, end) in file_offsets(file_size, chunks):
+        parse_chunk_async(file_name, start, end)
     
     total = LogCounter()
-    for (start, end) in file_offsets(os.path.getsize(file_name)):
-        partial = read_log_file(file_name, start, end)
-        total.add_counter(partial)
     
-    total.report()
+    stats.waiting()
+    for (counter, job_pid, job_time) in queue:
+        stats.received_job_result()
+        stats.job_report(job_pid, job_time)
+        
+        if counter:
+            total.add_counter(counter)
+        
+        stats.waiting()
+    
+    return total
 
 if __name__ == '__main__':
-    # import cProfile
-    # import pstats
-    # cProfile.Profile.bias = 1.1190049099519999e-05
-    # cProfile.run('main()', 'profile.data')
-    # pstats.Stats('profile.data').sort_stats('cumulative').print_stats()
-    main()
+    args = parse_argv()
+    
+    stats = Stats(args.log_file, log_each_job=args.log_each_job)
+    counter = parse_file(args.filename, args.cores, args.jobs_per_core, stats)
+    
+    stats.begin_report()
+    if not args.quiet:
+        counter.report()
+    
+    stats.done_report()
+    stats.report_master_stats()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import time
+import os
 from optparse import OptionParser
 
 def seek_open(file_name, start=0, end=-1):
@@ -21,25 +23,70 @@ def file_offsets(file_size, chunks):
     offsets.append(file_size)
     return map(lambda s, e: (s, e), offsets[:-1], offsets[1:])
 
-def report_job_time(start, end, time):
-    return "%d -> %d: %.3f" % (start, end, time)
-
 def parse_argv():
     parser = OptionParser()
     parser.add_option("-q", "--quiet", dest="quiet", action='store_true', default=False)
     parser.add_option("-c", "--cores", dest="cores", type='int', default=1)
     parser.add_option("-j", "--jobs-per-core", type='float', dest='jobs_per_core', default=1.)
     parser.add_option("-l", "--log-file", dest='log_file', default=None)
+    parser.add_option("--log-each-job", dest='log_each_job', action='store_true', default=False)
     (options, args) = parser.parse_args()
     
     options.filename = args[0]
     
     return options
 
-def log_message(file_name, message):
-    if file_name == '-':
-        print message
-    else:
-        f = open(file_name, 'a')
-        f.write(message + "\n")
-        f.close()
+class Stats:
+    def __init__(self, log_file, log_each_job=False):
+        self.wait_time = 0
+        self.join_result_time = 0
+        self.total_jobs_time = 0
+        self.prev_time = None
+        self.log_file = log_file
+        self.report_time = 0
+        self.log_each_job = log_each_job
+    
+    def initial_report(self, file_name, file_size, chunks):
+        self.n_jobs = chunks
+        self.begin_time = time.clock()
+        self._output("[master process] (%d) starting %s (%.1f MB: %d chunks of %.1f MB each)" %
+                (os.getpid(), file_name, file_size / 2**20, chunks, float(file_size) / chunks / 2**20))
+    
+    def waiting(self):
+        now_time = time.clock()
+        if self.prev_time:
+            self.wait_time += now_time - self.prev_time
+        self.prev_time = now_time
+    
+    def received_job_result(self):
+        now_time = time.clock()
+        self.join_result_time += now_time - self.prev_time
+        self.prev_time = now_time
+    
+    def job_report(self, job_pid, job_time):
+        if self.log_each_job:
+            self._output("[job %d] %.3f" % (job_pid, job_time))
+        self.total_jobs_time += job_time
+    
+    def report_master_stats(self):
+        self._output("[master process] (%d) done. total time: %.3f; avg job time: %.3f" %
+                (os.getpid(), time.clock() - self.begin_time, self.total_jobs_time / self.n_jobs))
+        self._output("[master process] idle time: %.3f; join time: %.3f; report time: %.3f" %
+                (self.wait_time, self.join_result_time, self.report_time))
+    
+    def begin_report(self):
+        self.report_start = time.clock()
+    
+    def done_report(self):
+        self.report_time = time.clock() - self.report_start
+    
+    def _output(self, message):
+        if not self.log_file:
+            return
+        
+        if self.log_file == '-':
+            print message
+        else:
+            f = open(self.log_file, 'a')
+            f.write(message + "\n")
+            f.close()
